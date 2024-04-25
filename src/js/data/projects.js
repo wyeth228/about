f150fe9a20e80a7d237a9074d048f20628889e37
Wyeth <contact.wyeth@gmail.com>
2024-04-25 17:58:14 +0300

    changes

--- src/js/data/projects.js
+++ src/js/data/projects.js
@@ -3,7 +3,7 @@ export var projects = [
 		title: "Weather Knower",
 		information: {
 			en: "Simple weather web application realized using React and some weather api service",
-			ru: "Простое web приложение для просмотра погоды"
+			ru: "Web приложение для просмотра погоды"
 		},
 		imageSourcePath: "./src/assets/images/projects/weather_knower.png",
 		link: "https://wyeth228.github.io/weather-knower/"
@@ -21,7 +21,7 @@ export var projects = [
 		title: "G4560",
 		information: {
 			en: "Pixel art animation",
-			ru: "Пиксель арт анимация"
+			ru: "Пиксель арт анимэйшн"
 		},
 		imageSourcePath: "./src/assets/images/projects/g4560.gif",
 		link: ""
@@ -29,8 +29,8 @@ export var projects = [
 	{
 		title: "Gym room",
 		information: {
-			en: "Simple gym room with some trainers",
-			ru: "Простой спортзал с тренажерами"
+			en: "3D gym visualization",
+			ru: "3D визуализация спортзала"
 		},
 		imageSourcePath: "./src/assets/images/projects/gym.png",
 		link: ""
@@ -38,8 +38,8 @@ export var projects = [
 	{
 		title: "Ping pong room",
 		information: {
-			en: "Ping pong room",
-			ru: "Комната для игры в пинг понг"
+			en: "3D ping pong room render",
+			ru: "3D визуализация комнаты для игры в пинг понг"
 		},
 		imageSourcePath: "./src/assets/images/projects/ping_pong.png",
 		link: ""
