93590018dd5d3b9ba208a368ed1ce5506aed6b5b
Wyeth <contact.wyeth@gmail.com>
2024-06-03 21:07:56 +0300

    changes

--- src/js/app/project_popup/swipeSlideRight.js
+++ src/js/app/project_popup/swipeSlideRight.js
@@ -3,29 +3,28 @@
  * @param {object} alreadyLoadedImages
  */
 export default function (nextImageSrc, alreadyLoadedImages) {
-  if (this.viewState.currentSlide.value < this.viewState.slidesTotal.value) {
-    this.viewState.currentSlide.value++;
+  if (this.viewState.currentSlide.value >= this.viewState.slidesTotal.value) {
+    return;
+  }
 
+  this.viewState.currentSlide.value++;
+
+  if (nextImageSrc in alreadyLoadedImages) {
+    this.viewState.image.value = nextImageSrc;
+  } else {
     this.viewState.loading.hidden = false;
 
     this.viewState.image.hidden = true;
 
-    if (nextImageSrc in alreadyLoadedImages) {
+    var img = new Image();
+    img.src = nextImageSrc;
+    img.onload = () => {
+      alreadyLoadedImages[nextImageSrc] = img;
+
       this.viewState.image.value = nextImageSrc;
       this.viewState.image.hidden = false;
 
       this.viewState.loading.hidden = true;
-    } else {
-      var img = new Image();
-      img.src = nextImageSrc;
-      img.onload = () => {
-        alreadyLoadedImages[nextImageSrc] = img;
-
-        this.viewState.image.value = nextImageSrc;
-        this.viewState.image.hidden = false;
-
-        this.viewState.loading.hidden = true;
-      };
-    }
+    };
   }
 }
